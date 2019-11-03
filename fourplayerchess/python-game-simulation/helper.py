05c7f239705b33216a873cb43abb23a310bdbd92
Jade <53711128+RazorA2@users.noreply.github.com>
2019-11-03 06:31:26 -0500

    Add next color function.

--- fourplayerchess/python-game-simulation/helper.py
+++ fourplayerchess/python-game-simulation/helper.py
@@ -1,1 +1,13 @@
+colors = {}
+colors["R"] = 0
+colors["B"] = 1
+colors["Y"] = 2
+colors["G"] = 3
 
+colorsRet = ["G", "B", "Y", "G"]
+
+
+# @function nextColor
+# Get the next color based on turn color
+def nextColor(turn):
+    return colorsRet[((colors[turn] + 1) % 4)]
